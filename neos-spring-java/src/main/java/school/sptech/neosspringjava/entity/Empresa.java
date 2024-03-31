7553288b4694c28e962f2a5e9c0ceafcb203125f
Gabriel Yukio <126337393+GabrielYukioMC@users.noreply.github.com>
2024-03-31 13:41:36 -0300

    sistema conectado ao banco de dados e com correções

---
+++ neos-spring-java/src/main/java/school/sptech/neosspringjava/entity/Empresa.java
@@ -0,0 +1,29 @@
+package school.sptech.neosspringjava.entity;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private int idEmpresa;
+    @Getter
+    @Setter
+    @NotBlank(message = "Nome é obrigatório")
+    @NotEmpty
+    private String nome;
+    @Getter
+    @Setter
+    @CNPJ(message = "CNPJ inválido")
+    private String cnpj;
+
+}
--- neos-spring-java/src/main/java/school/sptech/neosspringjava/modal/Client.java
+++
@@ -1,9 +0,0 @@
-package school.sptech.neosspringjava.modal;
-
-
-public class Client {
-    private Integer id;
-    private String name;
-    private String email;
-    private String password;
-}
