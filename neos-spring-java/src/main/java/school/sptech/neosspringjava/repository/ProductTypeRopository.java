db172f1173d4bdc74ba687ed7e83e33e0e08537b
LuizMiguelAdS <luiz.asilva@sptech.school>
2024-04-06 18:19:22 -0300

    Repositorys criados

---
+++ neos-spring-java/src/main/java/school/sptech/neosspringjava/repository/ProductTypeRopository.java
@@ -0,0 +1,9 @@
+package school.sptech.neosspringjava.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import school.sptech.neosspringjava.entity.ProductType;
+
+public interface ProductTypeRopository extends JpaRepository <ProductType, Integer>{
+
+}
---
+++ neos-spring-java/src/main/java/school/sptech/neosspringjava/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package school.sptech.neosspringjava.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import school.sptech.neosspringjava.entity.Service;
+
+public interface ServiceRepository extends JpaRepository<Service, Integer>{
+
+}
