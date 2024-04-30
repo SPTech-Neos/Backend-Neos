3c09003368eae27fe78fe0b1433f64d6b485afe8
LuizMiguelAdS <luiz.asilva@sptech.school>
2024-04-30 09:46:16 -0300

    csv organization esqueleto concluido

--- neos-spring-java/src/main/java/school/sptech/neosspringjava/domain/repository/productRepository/ProductRepository.java
+++ neos-spring-java/src/main/java/school/sptech/neosspringjava/domain/repository/productRepository/ProductRepository.java
@@ -1,9 +1,14 @@
 package school.sptech.neosspringjava.domain.repository.productRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import school.sptech.neosspringjava.domain.model.product.Product;
 
 public interface ProductRepository  extends JpaRepository<Product, Integer>{
-
+//@Query("SELECT p FROM Product p WHERE p.id = :productId AND p.data BETWEEN :startDate AND :endDate")
+List<Product> findByIdAndDataBetween(Long productId, LocalDate startDate, LocalDate endDate);
 }
