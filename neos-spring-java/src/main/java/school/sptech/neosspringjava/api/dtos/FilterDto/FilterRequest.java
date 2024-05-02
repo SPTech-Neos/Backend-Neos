52f7fc19e404d515c068cb7e5e3307efa983fcbc
Gabriel Yukio <126337393+GabrielYukioMC@users.noreply.github.com>
2024-05-02 00:02:49 -0300

    Dtos - correcao do value - price

--- neos-spring-java/src/main/java/school/sptech/neosspringjava/api/dtos/FilterDto/FilterRequest.java
+++ neos-spring-java/src/main/java/school/sptech/neosspringjava/api/dtos/FilterDto/FilterRequest.java
@@ -3,6 +3,6 @@ package school.sptech.neosspringjava.api.dtos.FilterDto;
 import lombok.Builder;
 
 @Builder
-public record FilterRequest(Double value, Integer fkEstablishment, Integer fkService) {
+public record FilterRequest(Double price, Integer fkEstablishment, Integer fkService) {
 
 }
