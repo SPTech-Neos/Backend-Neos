0d65bc3acca49f15d93b4e81987a44d64c518093
KevinRodriguesSilva <kevin.silva@sptech.school>
2024-06-08 10:47:03 -0300

    fix/correções do merge

--- neos-spring-java/src/main/java/school/sptech/neosspringjava/NeosSpringJavaApplication.java
+++ neos-spring-java/src/main/java/school/sptech/neosspringjava/NeosSpringJavaApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NeosSpringJavaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NeosSpringJavaApplication.class, args);
 	}
 
-}
+}	
